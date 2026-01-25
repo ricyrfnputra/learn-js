d5568063ab518bfbb63c32fcdd92e7bd38dc9076
Ricy <ricyrfnputra51@gmail.com>
2026-01-25 20:35:17 +0700

    feat: compare age using nested if

--- JS/logic/main.js
+++ JS/logic/main.js
@@ -43,3 +43,70 @@ if(person2.umur > person3.umur){
 };
 
 console.info(message);
+
+
+// Defined object
+const orang = {
+    nama: "Ricy"
+}; 
+
+if(orang.nama === undefined){
+    console.info("Nama belum di isi")
+} else {
+    console.info(`Hello ${orang.nama} selamat bergabung`)
+};
+
+
+
+// Dalam Switch
+
+const wong1 = {
+    nama: "Gabriella",
+    umur: 25
+};
+
+const wong2 = {
+    nama: "Zahra",
+    umur: 20
+};
+
+// switch (wong.nomo){
+//     case undefined:
+//     case "":
+//         console.info("nama belum diisi");
+//         break;
+//     default:
+//         console.info(`Hello ${wong.nama} selamat datang`);
+// }
+
+// ! 
+if(!wong1.nomo){
+    console.info("Nama belum disi")
+} else {
+    console.info(`Hello ${wong1.nomo}`);
+}
+
+
+// contoh di nested
+
+if(!wong1.umur || !wong2.umur) {
+    console.info("Data umur salah satu orang belum di isi");
+} else {
+    // Nested if
+    if(wong1.umur === wong2.umur) {
+        console.info(`${wong1.nama} dan ${wong2.nama} seumuran`);
+    } else {
+        if(wong1.umur > wong2.umur){
+            console.info(`${wong1.nama} lebih tua dari ${wong2.nama}`)
+        } else {
+            console.info(`${wong2.nama} lebih tua dari ${wong1.nama}`)
+        }
+    }
+};
+
+
+
+
+
+
+
