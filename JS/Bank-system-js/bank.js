a8f2f528998dffad83afe95b32d45a715e56e65c
Ricy <ricyrfnputra51@gmail.com>
2026-02-05 22:02:31 +0700

    fix logic grade

--- JS/Bank-system-js/bank.js
+++ JS/Bank-system-js/bank.js
@@ -1,42 +1,45 @@
     let saldo = prompt("Masukan saldo lu");
+    let riwayatTransaksi = [];
 
     const cek = document.getElementById("cek-saldo");
     const tarik = document.getElementById("tarik-tunai");
     const setor = document.getElementById("setor-tunai");
     const batal = document.getElementById("batal");
-
+    const listRiwayat = document.getElementById("riwayat");
 
     cek.addEventListener("click", () => {
-        alert(`Saldo lu sekarang adalah: Rp ${saldo.toLocaleString('id-ID')}`);
+    alert(`Saldo lu sekarang adalah: Rp ${saldo.toLocaleString("id-ID")}`);
     });
 
     setor.addEventListener("click", () => {
-        const jumlahStor = parseInt(prompt("Lu mau setor berape?"));
+    const jumlahStor = parseInt(prompt("Lu mau setor berape?"));
 
-        if(!isNaN(jumlahStor) && jumlahStor > 0) {
-            saldo += jumlahStor;
-            alert(`setor tunai berhasil! saldo lu sekarang: Rp ${saldo.toLocaleString('id-ID')}`);
-        } else {
-            alert("Input gak valid.");
-        }
+    if (!isNaN(jumlahStor) && jumlahStor > 0) {
+        saldo += jumlahStor;
+        alert(`setor tunai berhasil! saldo lu sekarang: Rp ${saldo.toLocaleString("id-ID")}`);
+    } else {
+        alert("Input gak valid.");
+    }
     });
+    d;
 
     tarik.addEventListener("click", () => {
-        const jumlahTarik = parseInt(prompt("mau narik berape lu? "));
+    const jumlahTarik = parseInt(prompt("mau narik berape lu? "));
 
-        if(isNaN(jumlahTarik) || jumlahTarik < 0){
-            alert("input tidak valid");
-        } else if (jumlahTarik > 0) {
-            saldo -= jumlahTarik;
-            alert(`saldo anda sekarang sebesar: Rp ${saldo.toLocaleString('id-ID')}`);
-        } else {
-            alert("input salah")
-        };
+    if (isNaN(jumlahTarik) || jumlahTarik < 0) {
+        alert("input tidak valid");
+    } else if (jumlahTarik > 0) {
+        saldo -= jumlahTarik;
+        alert(`saldo anda sekarang sebesar: Rp ${saldo.toLocaleString("id-ID")}`);
+    } else {
+        alert("input salah");
+    }
     });
 
     batal.addEventListener("click", () => {
-        alert("transaksi dibatalkan, terimakasih sudah menggunakan layanan kami");
+    alert("transaksi dibatalkan, terimakasih sudah menggunakan layanan kami");
     });
+
 
 
 
