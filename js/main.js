9f9db4eaaca3e2bab46a5ad66d3be72926ce978d
Ricy Rifani Putra <ricyrfnputra51@gmail.com>
2026-04-08 23:30:11 +0700

    add file js, array method in js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// * + * WELCOME TO MY PLAYGROUND * + *
+
+// arrow function
+//const mahasiswa = ["jacky", "chan", "john", "cena"];
+// console.log(mahasiswa.map((mahasiswa) => mahasiswa.length));
+// mahasiswa.push("angel");    // add array
+// console.log(mahasiswa);
+
+// const lastName = mahasiswa.pop();   // removes the last element
+// console.log(mahasiswa);
+ 
+// default paramters
+// function kali(a, b) {
+//     return a * b;
+// }
+
+// kali(5, 2);
+
+
+
+// * * * * * ARRAY * * * * * 
+// Arrow Function
+const sapa = (nama) => "Halow " + nama;
+
+console.log(sapa("Budi"));
